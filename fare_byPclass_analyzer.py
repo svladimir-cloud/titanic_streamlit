ea49ddedb502b7296252ea97693a76bc6616f426
Vladimir <vladimir.suter@yandex.ru>
2026-06-11 06:45:56 +0500

    Добавление анализа выживаемости

--- fare_byPclass_analyzer.py
+++ fare_byPclass_analyzer.py
@@ -24,3 +24,31 @@ def calculate_total_fare_by_class(df: pd.DataFrame, selected_sex: str):
     fare_summary.columns = columns
 
     return fare_summary
+
+def calculate_survival_rate(df: pd.DataFrame, selected_sex: str):
+    """Рассчитывает процент выживших по классу обслуживания."""
+    check_required_columns(df)
+    
+    required = ['Sex', 'Pclass', 'Survived']
+    if not all(col in df.columns for col in required):
+        raise ValueError(f"df должен содержать колонки: {required}")
+    
+    filtered_df = df[df['Sex'] == selected_sex]
+    
+    survival_summary = (
+        filtered_df
+        .groupby('Pclass')['Survived']
+        .agg(['count', 'sum'])
+        .reset_index()
+    )
+    survival_summary['survival_rate'] = (
+        survival_summary['sum'] / survival_summary['count'] * 100
+    ).round(2)
+    
+    survival_summary.columns = [
+        'Класс обслуживания',
+        'Всего пассажиров',
+        'Выжило',
+        'Процент выживших'
+    ]
+    return survival_summary.drop(columns=['Всего пассажиров', 'Выжило'])
